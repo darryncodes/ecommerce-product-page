d62a9401481a764c326ca7f586af495569e01344
Darryn Codes <darryncodes@gmail.com>
2023-04-16 14:14:16 +0100

    ui - adding grid for product layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/UI/Header";
+import Grid from "./components/UI/Grid";
 
 function App() {
     return (
         <div className="max-w-[68.75rem] mx-auto">
             <Header />
+            <Grid />
         </div>
     );
 }
---
+++ src/components/UI/Grid.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Grid = () => {
+    return (
+        <main className="mb-8 grid grid-cols-1 md:grid-cols-2 md:mb-0 md:gap-16 md:py-24 md:mx-4 md:place-items-center"></main>
+    );
+};
+
+export default Grid;
